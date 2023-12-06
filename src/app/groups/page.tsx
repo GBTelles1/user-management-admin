d0b710665b2becca62b2fb9b96fc8d5cc66901be
Gabriel Telles <gbertelles@gmail.com>
2023-12-06 14:04:11 -0300

    It creates the page /groups

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -1,5 +1,31 @@
+'use client';
+import { MainContainer } from '@/styles/Home';
+import { CreateEntityButton } from '../components/CreateUserButton';
+import { GroupsDataTable } from '../components/GroupsDataTable';
+
+const groups = [
+  {
+    id: 1,
+    name: 'Gabriel Group',
+    users: ['bwrvw23w23f22', 'bwrvw23w23f23', 'bwrvw23w23f24']
+  },
+  {
+    id: 2,
+    name: 'Carol Group',
+    users: ['bwrvw23w23f23']
+  },
+  {
+    id: 3,
+    name: 'Neném Group',
+    users: ['bwrvw23w23f22', 'bwrvw23w23f23']
+  },
+];
+
 export default function GroupsPage() {
   return (
-    <div>GroupsPage</div>
+    <MainContainer>
+      <CreateEntityButton dataType='groups' />
+      <GroupsDataTable groups={groups}/>
+    </MainContainer>
   );
 }
