37f2667db55a5ae1ca7f7f3590d37891226f2da9
Gabriel Telles <gbertelles@gmail.com>
2023-12-06 10:30:10 -0300

    Add responsiveness, Clean Code, and Accessability Also add some navigation link style

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -3,14 +3,21 @@ import Image from 'next/image';
 import { HeaderContainer } from './styles';
 import interNationsLogo from '../../../../public/InterNationsLogo.svg';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export function Header() {
+  const pathname = usePathname();
+
+  const isHomePage = pathname == '/' ? 'active' : '';
+  const isGroupsPage = pathname == '/groups' ? 'active' : '';
+  
   return (
     <HeaderContainer>
-      <Image src={interNationsLogo} alt=''/>
+      <Image src={interNationsLogo} alt='InterNations Logo'/>
+
       <nav>
-        <Link href={'/'}>Members</Link>
-        <Link href={'/groups'}>Groups</Link>
+        <Link href={'/'} className={isHomePage}>Members</Link>
+        <Link href={'/groups'} className={isGroupsPage}>Groups</Link>
       </nav>
     </HeaderContainer>
   );
