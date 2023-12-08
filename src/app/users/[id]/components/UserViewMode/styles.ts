03c76bd355a6c8e339524b5b0177e1a9fbab7c57
Gabriel Telles <gbertelles@gmail.com>
2023-12-08 14:33:06 -0300

    Create the component UserViewMode

---
+++ src/app/users/[id]/components/UserViewMode/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const UserDetail = styled.div`
+  width: 40%;
+  padding: 1rem;
+
+  border: 2px solid #000000;
+  border-radius: 10px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
